399176668b0c0d72ed91205b22ea3c32040a5a2e
moonk <mkjoehsb@gmail.com>
2022-01-12 19:06:23 +0900

    sending js from python
    and screenshot

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 from webdriver_manager.chrome import ChromeDriverManager
-
+KEYWORD="buy domain"
 browser = webdriver.Chrome(ChromeDriverManager().install())
 browser.get("https://google.com")
 
 search_bar = browser.find_element_by_class_name("gLFyf")
 print(search_bar)
 
-search_bar.send_keys("hello")
+search_bar.send_keys(KEYWORD)
 search_bar.send_keys(Keys.ENTER)
 
-search_results = browser.find_element_by_id("rso").find_elements_by_class_name("g")
+shitty_element = WebDriverWait(browser,10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "AuVD"))
+)
+
+browser.execute_script(
+    """
+    console.log(arguments);
+    const shitty = arguments[0];
+    console.log(shitty);
+    shitty.parentElement.removeChild(shitty)
+    """,
+    shitty_element,
+)
+
+
+
+
+
+
+#search_results = browser.find_element_by_id("rso").find_elements_by_class_name("g")
+search_results = browser.find_elements_by_class_name("g")
 print(search_results)
 
-for search_result in search_results:
-    title = search_result.find_element_by_tag_name("h3");
-
-    if(title):
-        print("***********")
-        print(title.text)
+#shitty_element = browser.find_element_by_class_name('g jNVrwc Y4pkMc')
+for index, search_result in enumerate (search_results):
+    #search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
+    search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
+browser.quit()
