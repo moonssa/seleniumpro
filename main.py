99e2acd944821499517d3011657b1bc063048b5c
moonk <mkjoehsb@gmail.com>
2022-01-13 23:43:15 +0900

    screenshot code clean up

--- main.py
+++ main.py
@@ -7,41 +7,58 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 from webdriver_manager.chrome import ChromeDriverManager
-KEYWORD="buy domain"
-browser = webdriver.Chrome(ChromeDriverManager().install())
-browser.get("https://google.com")
-
-search_bar = browser.find_element_by_class_name("gLFyf")
-print(search_bar)
-
-search_bar.send_keys(KEYWORD)
-search_bar.send_keys(Keys.ENTER)
-
-shitty_element = WebDriverWait(browser,10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "AuVD"))
-)
-
-browser.execute_script(
-    """
-    console.log(arguments);
-    const shitty = arguments[0];
-    console.log(shitty);
-    shitty.parentElement.removeChild(shitty)
-    """,
-    shitty_element,
-)
 
 
+class GoogleKeywordScreenshooter:
+    def __init__(self, keyword, screenshots_dir):
+
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        self.keyword=keyword
+        self.screenshots_dir = screenshots_dir
+
+    def start(self):
+        self.browser.get("https://google.com")        
+        search_bar = self.browser.find_element_by_class_name("gLFyf")
+        print(search_bar)
+
+        search_bar.send_keys(self.keyword)
+        search_bar.send_keys(Keys.ENTER)
+
+        try:
+            shitty_element = WebDriverWait(self.browser,10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "AuVD"))
+            )
+
+            self.browser.execute_script(
+                """
+                const shitty = arguments[0];
+                shitty.parentElement.removeChild(shitty)
+                """,
+                shitty_element,
+            )
+        except Exception:
+            pass
+
+        search_results = self.browser.find_elements_by_class_name("g")
+        print(search_results)
+
+        for index, search_result in enumerate (search_results):
+            search_result.screenshot(f"{self.screenshots_dir}/{self.keyword}x{index}.png")
+
+    def finish(self):
+        self.browser.quit()
+
+
+
+domain_competitors = GoogleKeywordScreenshooter("buy domain","screenshots")
+domain_competitors.start()
+domain_competitors.finish()
+
+
+python_competitors = GoogleKeywordScreenshooter("python book", "screenshots")
+python_competitors.start()
+python_competitors.finish()
 
 
 
 
-#search_results = browser.find_element_by_id("rso").find_elements_by_class_name("g")
-search_results = browser.find_elements_by_class_name("g")
-print(search_results)
-
-#shitty_element = browser.find_element_by_class_name('g jNVrwc Y4pkMc')
-for index, search_result in enumerate (search_results):
-    #search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
-    search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
-browser.quit()
