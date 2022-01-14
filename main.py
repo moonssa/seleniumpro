473b63614c507c4f565369b8e9ee9e7b1b3aee7d
moonk <mkjoehsb@gmail.com>
2022-01-14 09:51:21 +0900

    snapshot all searched page
    challenge

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 class GoogleKeywordScreenshooter:
-    def __init__(self, keyword, screenshots_dir):
+    def __init__(self, keyword, screenshots_dir, max_page=4):
 
         self.browser = webdriver.Chrome(ChromeDriverManager().install())
         self.keyword=keyword
         self.screenshots_dir = screenshots_dir
+        self.max_page = max_page
 
     def start(self):
         self.browser.get("https://google.com")        
@@ -39,11 +40,24 @@ class GoogleKeywordScreenshooter:
         except Exception:
             pass
 
-        search_results = self.browser.find_elements_by_class_name("g")
-        print(search_results)
 
-        for index, search_result in enumerate (search_results):
-            search_result.screenshot(f"{self.screenshots_dir}/{self.keyword}x{index}.png")
+        def screenshot_bot(search_results, page_num):
+            for index, search_result in enumerate(search_results):
+                search_result.screenshot(f"{self.screenshots_dir}/{page_num}-{self.keyword}x{index}.png")
+
+        def searching_bot():
+            search_results = self.browser.find_elements_by_class_name("g")
+            current_page = self.browser.find_element_by_class_name("YyVfkd")
+            next_page = self.browser.find_element_by_id("pnnext")
+            if int(current_page.text) < self.max_page and next_page :
+                screenshot_bot(search_results, int(current_page.text))
+                next_page.click()
+                searching_bot()
+            else:
+                screenshot_bot(search_results, int(current_page.text))
+
+        searching_bot()
+       
 
     def finish(self):
         self.browser.quit()
@@ -58,7 +72,3 @@ domain_competitors.finish()
 python_competitors = GoogleKeywordScreenshooter("python book", "screenshots")
 python_competitors.start()
 python_competitors.finish()
-
-
-
-
